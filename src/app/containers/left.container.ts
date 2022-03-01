83554b1ff0903eb07e28b40ff48102e4a4f957a4
Jackson Keet <jackson.keet@mac.com>
2022-03-01 13:02:44 +1300

    Added swipe function from deg/pg bar to planner

--- src/app/containers/left.container.ts
+++ src/app/containers/left.container.ts
@@ -54,8 +54,8 @@ import { MatTabsModule } from '@angular/material';
     </div>
   </div>
 
-  <div *ngIf="mobile" class="relative">
-  <mat-tab-group mat-align-tabs="start">
+  <div *ngIf="mobile" class="relative" (touchstart)="swipe($event, 'start')" (touchend)="swipe($event, 'end')">
+  <mat-tab-group mat-align-tabs="start" [(selectedIndex)]="selectedTab">
   <mat-tab label="First">
   <div class="panel-mobile">
     <progress-panel (onPageChange)="changePage()"></progress-panel>
@@ -78,6 +78,10 @@ export class LeftPanelContainer {
   private screenHeight;
   private screenWidth;
 
+  private swipeCoord;
+  private swipeTime;
+  private selectedTab = 0;
+
   constructor(private storeHelper: StoreHelper) {
     this.onResize();
   }
@@ -94,6 +98,38 @@ export class LeftPanelContainer {
        this.mobile = false;
      }
   }
+
+ private swipe(e: TouchEvent, when: string): void {
+    const coord: [number, number] = [e.changedTouches[0].clientX, e.changedTouches[0].clientY];
+    const time = new Date().getTime();
+   if (when === 'start') {
+    this.swipeCoord = coord;
+    this.swipeTime = time;
+    } else if (when === 'end') {
+    const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
+    const duration = time - this.swipeTime;
+   if (duration < 1000 //
+    && Math.abs(direction[0]) > 30 // Long enough
+    && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) { // Horizontal enough
+    const swipe = direction[0] < 0 ? 'next' : 'previous';
+    console.info(swipe);
+    if(swipe === 'next'){
+    const isFirst = this.selectedTab === 0;
+   if(this.selectedTab <= 3){
+    this.selectedTab = isFirst ? 1 : this.selectedTab + 1;
+   }
+   console.log("Swipe left — INDEX: " + this.selectedTab);
+    } else if(swipe === 'previous'){
+    const isLast = this.selectedTab === 1;
+   if(this.selectedTab >= 1){
+    this.selectedTab = this.selectedTab - 1;
+    }
+   console.log("Swipe right — INDEX: " + this.selectedTab);
+    }
+   // Do whatever you want with swipe
+    }
+    }
+   }
 
 
   private ngOnInit() {
