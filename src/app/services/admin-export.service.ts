fb4f46cabb69cbc80e5f598c9b8470611c032b7b
Jackson <jackson.keet@mac.com>
2022-09-19 22:25:16 +1200

    Added export date submitted

--- src/app/services/admin-export.service.ts
+++ src/app/services/admin-export.service.ts
@@ -1,41 +1,51 @@
-import { Injectable } from '@angular/core';
+import { formatDate } from "@angular/common";
+import { Injectable } from "@angular/core";
 import { Component, Input } from "@angular/core";
-import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireAuth } from "@angular/fire/auth";
 import { AngularFirestore } from "@angular/fire/firestore";
 import { AuthService } from "../core/auth.service";
 
 @Injectable()
 export class AdminExportService {
+  public adminStatus;
 
-    public adminStatus;
+  constructor(
+    public authService: AuthService,
+    public db: AngularFirestore,
+    public afAuth: AngularFireAuth
+  ) {}
 
-    constructor(public authService: AuthService, public db: AngularFirestore, public afAuth: AngularFireAuth,) {
-    }
+  public setExportStatus(adminStatus) {
+    this.db
+      .collection("users")
+      .doc("jackson.keet1989@gmail.com")
+      .update({ status: adminStatus });
+    this.adminStatus = adminStatus;
+  }
 
-    public setExportStatus(adminStatus) {    
-        this.db
+  public getStatus() {
+    return this.adminStatus;
+  }
+
+  public getExportStatus() {
+    return new Promise<any>((resolve) => {
+      var data = this.db
         .collection("users")
         .doc("jackson.keet1989@gmail.com")
-        .update({status: adminStatus})
-        this.adminStatus = adminStatus;
-      }
-
-    public getStatus() {
-        return this.adminStatus;
-      }
-
-      public getExportStatus() {
-        return new Promise<any>((resolve) => {
-          var data = this.db
-            .collection("users")
-            .doc("jackson.keet1989@gmail.com")
-            .get()
-            .toPromise()
-            .then((result) => {
-              resolve(this.adminStatus = result.data().status);
-            });
+        .get()
+        .toPromise()
+        .then((result) => {
+          resolve((this.adminStatus = result.data().status));
         });
-      }
-
+    });
   }
-  
+
+  public setExportStatusTimestamp() {
+    let timestamp = Date.now();
+    let timestampString = formatDate(timestamp, "dd/MM/yyyy, h:mm a", "en");
+      this.db
+      .collection("users")
+      .doc("jackson.keet1989@gmail.com")
+      .update({timestamp: timestampString})
+  }
+}
